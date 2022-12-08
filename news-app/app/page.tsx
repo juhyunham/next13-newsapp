89f1ddadd5e8e7931d7841dc89e41fef6e8ba48c
hamju <judev0227@gmail.com>
2022-12-08 14:47:01 +0900

    환경 세팅 및 app파일 생성

---
+++ news-app/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function Homepage() {
+  return (
+    <div>
+      <Header />
+      Homepage
+    </div>
+  );
+}
+
+export default Homepage;
